b4890dc355c2052d994452771d75d586779e048f
ZuhirL <lhdl.zhr@gmail.com>
2023-05-25 11:20:58 +0200

    Add StatusController

---
+++ src/main/java/com/example/springboot/controller/StatusController.java
@@ -0,0 +1,16 @@
+package com.example.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("status")
+public class StatusController {
+
+    @GetMapping(value = "/alive")
+    public String alive() {
+        return "OK";
+    }
+
+}
