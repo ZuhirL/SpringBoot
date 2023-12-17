a17548d9c35a4ca68c45aeda75c46c316a75cf56
ZuhirL <lhdl.zhr@gmail.com>
2023-12-18 00:03:46 +0100

    test

--- target/generated-sources/annotations/com/example/springboot/mapper/CdrMapperImpl.java
+++ target/generated-sources/annotations/com/example/springboot/mapper/CdrMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-17T23:59:36+0100",
+    date = "2023-12-18T00:02:58+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 @Component
