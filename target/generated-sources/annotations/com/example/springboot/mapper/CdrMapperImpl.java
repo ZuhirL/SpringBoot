dced9df749929c061fe5ef4cec5f42827dc06978
ZuhirL <lhdl.zhr@gmail.com>
2023-12-17 21:37:38 +0100

    added jacoco coverage test

--- target/generated-sources/annotations/com/example/springboot/mapper/CdrMapperImpl.java
+++ target/generated-sources/annotations/com/example/springboot/mapper/CdrMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-16T20:18:20+0100",
+    date = "2023-12-17T20:11:20+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 @Component
