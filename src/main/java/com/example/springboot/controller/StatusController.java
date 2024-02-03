67df3d817e9547afb0aa1697a6c671a1af5be489
ZuhirL <lhdl.zhr@gmail.com>
2024-02-03 12:21:50 +0100

    added healthz API

--- src/main/java/com/example/springboot/controller/StatusController.java
+++ src/main/java/com/example/springboot/controller/StatusController.java
@@ -1,12 +1,17 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.dao.CdrRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("status")
+@RequiredArgsConstructor
 public class StatusController {
+
+  private final CdrRepository cdrRepository;
 
   @GetMapping(value = "/alive")
   public String alive() {
@@ -15,6 +20,7 @@ public class StatusController {
 
   @GetMapping(value = "/healthz")
   public String healthz() {
+    cdrRepository.count();
     return "OK";
   }
 
