132f573f3ac20c7a1e574b57122d799d14d0c37b
ZuhirL <lhdl.zhr@gmail.com>
2024-05-27 18:50:45 +0200

    add auth singin exception handler

--- src/main/java/com/example/springboot/controller/AuthController.java
+++ src/main/java/com/example/springboot/controller/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,13 +29,13 @@ public class AuthController {
     private TokenProvider tokenService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody @Valid SignUpDto data) {
+    public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpDto data) {
         service.signUp(data);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<JwtDto> signIn(@RequestBody @Valid SignInDto data) {
+    public ResponseEntity<JwtDto> signIn(@RequestBody @Valid SignInDto data) throws AuthenticationException {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
         var authUser = authenticationManager.authenticate(usernamePassword);
         var accessToken = tokenService.generateAccessToken((User) authUser.getPrincipal());
